9094ca8591245e49a2c1bc1d02578df3a1db4bb6
ivan-ig <ivan_10_93@bk.ru>
2021-09-16 00:46:18 +0300

    Add Model

---
+++ src/main/java/com/github/ivan_ig/models/Person.java
@@ -0,0 +1,61 @@
+package com.github.ivan_ig.models;
+
+import javax.validation.constraints.*;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    @NotEmpty(message = "The phone number should not be empty")
+    @Pattern(regexp = ".{11}", message = "The phone number should be 11 digits long")
+    @Pattern(regexp = "\\d{11}", message = "The phone number should be digits only")
+    private String phoneNumber;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    public Person(int id, String name, String phoneNumber, String email) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Person() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
